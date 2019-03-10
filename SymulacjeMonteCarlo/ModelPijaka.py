01c4eb9a4da63d07f8c90aa30533c11d3bf85db3
236780 <236780@student.pwr.edu.pl>
2019-03-11 00:01:11 +0100

    added stddev calculation

--- SymulacjeMonteCarlo/ModelPijaka.py
+++ SymulacjeMonteCarlo/ModelPijaka.py
@@ -1,40 +1,36 @@
-import cupy as cp
+
 import numpy as np
+
+import cupy as cp #comment this line to disable CUDA
+#cp = np          #uncomment this line to disable CUDA
+
 import time
+
 #CONFIG
-n_samples = 100000 #sample size for one simulation
+n_samples = 1000000 #sample size for one simulation
 n_steps_min = 1    #simulations for number of steps from 10**n_steps_min to 10**n_steps_max
-n_steps_max = 6
-n_datapoints = 20  #number of simulations
-
+n_steps_max = 5
+n_datapoints = 10  #number of simulations
 outputFileName = "output.dat"
-
-
 
 n_steps_array=np.logspace(n_steps_min,n_steps_max,n_datapoints,True).astype(np.int32)
 
 fileOut=open(outputFileName,'w')
 
-t0=time.time()
 for n_steps in n_steps_array:
-    fileOut.write(f'\n\n{n_steps}\n')
+    t0=time.time()
     positions = cp.zeros(int(n_samples),np.int32)
-    distribution = cp.zeros(2*int(n_steps)+1,np.int32)
-
+   
     for n in range(n_steps):
         rand = cp.random.random_integers(0,1,n_samples)
         rand = 2*rand-1
         positions = cp.add(positions,rand)
 
-
-    for x in positions:
-        distribution[x+n_steps] += 1
-    for i in range(-n_steps,n_steps+1):
-        if(distribution[i+n_steps]!=0):
-            fileOut.write(f'{i} {distribution[i+n_steps]}\n')
+    mean = positions.mean()
+    std = positions.std()
+    t1 = time.time()
+    t=t1-t0
+    print(f'{n_steps}\t{mean}\t{std}\t{t}')
+    fileOut.write(f'{n_steps}\t{mean}\t{std}\t{t}\n')
 
 fileOut.close()
-
-t1 = time.time()
-t=t1-t0
-print(t)
