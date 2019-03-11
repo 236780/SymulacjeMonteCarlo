948ab598a6f57094ecdacfb822e4e9d523bae13c
236780 <236780@student.pwr.edu.pl>
2019-03-11 18:25:56 +0100

    Add boundary conditions to MonteCarlo2.py

--- MonteCarlo2/MonteCarlo2.py
+++ MonteCarlo2/MonteCarlo2.py
@@ -1,39 +1,40 @@
 import numpy as np
 import random
 
-n = 5 #number of atoms
-l = 20 #dimension of the box
-kmax = 100 #number of steps
-
+#Konfiguracja
+n = 30       #liczba atomów
+l = 20      #rozmiar układu
+kmax = 100  #liczba kroków
 outputFileName = "DiffusionCoefficient.out"
+#------------------------------------------
 
-box = np.zeros((l,l),bool)
-r0_array = np.ndarray((n,2),int)
+box = np.zeros((l,l),bool)          #macierz przechowuje informacje o tym czy miejsce jest zajęte
+r0_array = np.ndarray((n,2),int)    #macierz położeń początkowych
 
+#losowanie położeń początkowych
 for [x,y] in r0_array:
     [x,y] = np.random.randint(0,l,(2))
     while (box[x,y]==True):
         [x,y] = np.random.randint(0,l,(2))
     box[x,y]=True
+#-----------------------------------------
 
-r_array = r0_array
-
+r_array = r0_array  #macierz położeń atomów
 outputFile = open(outputFileName,"w")
 
-#TODO: warunki brzegowe!
-
-
 for k in range(1,kmax+1):
+
     for atom_r in r_array:
-        dr = random.choice([[1,0],[-1,0],[0,1],[0,-1]])
-            
-            if (box[atom_r+dr] == False):
-                box[atom_r] = False
-                atom_r = atom_r + dr
-                box[atom_r] = True
-    
+        dr = random.choice([[1,0],[-1,0],[0,1],[0,-1]]) #losowanie kierunku przemieszczenia
+        if (atom_r[0]>=0 and atom_r[0]<l and atom_r[1]>=0 and atom_r[1]<l \
+         and box[atom_r+dr] == False):
+            box[atom_r] = False
+            atom_r = atom_r + dr
+            box[atom_r] = True
+
     delta_r_array = r_array - r0_array
     delta_r2 = [float(x)**2+float(y)**2 for [x,y] in delta_r_array]
     delta_r2_mean = np.mean(delta_r2)
     diffusion_coefficient = delta_r2_mean / (4*k)
+
     outputFile.write(f'{k}\t{delta_r2_mean}\t{diffusion_coefficient}')
